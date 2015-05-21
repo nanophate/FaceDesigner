ade03007a5b950316e7a12ca9b850f9201c427c8
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-05-21 12:18:19 +0000

    correct the javascript passing

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,6 +1,6 @@
 if (window.console) {
-  console.log();
- /*
+/*  console.log();
+ *
  *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
