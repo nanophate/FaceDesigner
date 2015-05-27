fc6dd71003710bd85164c368301743e6e5874bcb
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-05-27 14:24:44 +0000

    del suggested 0

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -6,7 +6,7 @@ var constraints = {  //limit data going
   video: true
 };
 //preparing web socket
-var ws = new WebSocket("ws://face-designer.herokuapp.com:8080");
+var ws = new WebSocket("ws://face-designer.herokuapp.com:8080"); //ref to qiitq 3*
   ws.onopen = function () {
   console.log("Openened connection to websocket");
   }
@@ -23,16 +23,15 @@ navigator.getUserMedia(constraints, successCallback, errorCallback);
 //capture the video and snap invervaly then send the data using websocket to the server
 timer = setInterval(  
       function () {
-        //cheating to get the jq method done
+        //cheating to get the jq method done 2*
         $(function() {
-          var ctx = canvas.get(0)[0].getContext('2d');  //d.graph similar thing
+          var ctx = canvas.get()[0].getContext('2d');  //d.graph similar thing
           ctx.drawImage(video, 0, 0, 320, 240);
           var data = canvas.get()[0].toDataURL('image/jpeg', 1.0);
-          newblob = dataURItoBlob(data);
+          newblob = dataURItoBlob(data); //1*
           ws.send(newblob);
         })
       }, 250);
-
-
-
-//http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158
+// 1* http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158
+// 2* http://stackoverflow.com/questions/10291693/cannot-read-property-getcontext-of-null-using-canvas
+// 3* http://qiita.com/yyyske/items/0d00e5e352b7078a501e
