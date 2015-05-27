9fa102d295d74a9734cc7133045a2c7a02da011b
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-05-27 13:11:12 +0000

    webkit to window.url

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,13 +1,13 @@
-video = document.querySelector('video');
-var constraints = {
+video = document.querySelector('video');  //look for tags indicated video
+var constraints = {  //limit data going
   audio: false,
   video: true
 };
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;  
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;  //Multiple web broser support
       function successCallback(stream) {
-        video.src = webkitURL.createObjectURL(stream);
+        video.src = URL.createObjectURL(stream);  //get thing the src"http://asskao0-..."
       }
       function errorCallback(error) {
         console.log("Unable to get video stream!")
       }
-navigator.getUserMedia(constraints, successCallback, errorCallback);
+navigator.getUserMedia(constraints, successCallback, errorCallback);  //main thats doing the function and the job
