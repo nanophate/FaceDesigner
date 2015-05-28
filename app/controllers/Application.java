7bf3ec9b9b937b94993bf3f41e30076c3192d261
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-05-28 03:21:24 +0000

    add the model

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,7 @@ import play.*;
 import play.mvc.*;
 import play.mvc.Result;
 import play.mvc.WebSocket;
-import models.t;
+//import models.t;
 
 import views.html.*;
 
@@ -13,39 +13,9 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render());
     }
-    public static WebSocket<String> index() {
-    return new WebSocket<String>() {
-
-        // Called when the Websocket Handshake is done.
-        public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
-
-            // For each event received on the socket,
-            in.onMessage(new Callback<String>() {
-                public void invoke(String event) {
-
-                    // Log events to the console
-                    System.out.println(event);
-
-                }
-            });
-
-            // When the socket is closed.
-            in.onClose(new Callback0() {
-                public void invoke() {
-
-                    System.out.println("Disconnected");
-
-                }
-            });
-
-            // Send a single 'Hello!' message
-            out.write("Hello!");
-
-        }
-
-    };
-}
     public static Result videoJs(){
         return ok(views.js.video.render());
     }
 }
+//https://devcenter.heroku.com/articles/play-java-websockets
+//https://www.playframework.com/documentation/2.2.x/JavaWebSockets
