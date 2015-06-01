b782fdd36766dc93505002883eedf046774a7d8d
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-06-01 05:04:02 +0000

    websocket setup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,6 +16,22 @@ public class Application extends Controller {
     public static Result videoJs(){
         return ok(views.js.video.render());
     }
+    public static WebSocket<JsonNode> index)(dataBlob) {
+      return new WebSocket<JsonNode>() {
+
+        
+        // Called when the Websocket Handshake is done.
+        public void onReady(WebSocket.In<JsonNode> in, WebSocket.Out<JsonNode> out){
+
+            /*// Join the chat room.
+            try { 
+                ChatRoom.join(username, in, out);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }*/
+        }
+    };
+}
 }
 //https://devcenter.heroku.com/articles/play-java-websockets
 //https://www.playframework.com/documentation/2.2.x/JavaWebSockets
