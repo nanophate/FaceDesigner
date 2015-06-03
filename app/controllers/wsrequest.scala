3ad4791e72a83aaf3155de14e1a31ab6092a57f7
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-06-03 14:38:37 +0000

    scala file

--- app/controllers/wsrequest.scala
+++ app/controllers/wsrequest.scala
@@ -1,4 +1,13 @@
-def wsrequest = WebSocket.using[Array[Byte]] { request =>
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.api.libs.iteratee._
+
+object wsrequest extends Controller {
+
+  def wsrequest = WebSocket.using[Array[Byte]] { request =>
  
   // Create the outbound value that is called for each
   val out = Enumerator.imperative[Array[Byte]]();
