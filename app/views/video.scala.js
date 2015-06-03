3dd569455aa442547892e5880a4eff709f5ce48d
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-06-03 12:53:19 +0000

    bracket add for wss browser support

--- app/views/video.scala.js
+++ app/views/video.scala.js
@@ -26,7 +26,7 @@ function dataURItoBlob(dataURI) {
     return new Blob([ia], {type:mimeString});
 }
 //preparing web socket
-var WSS =  window['MozWebSocket'] ? MozWebSocket : WebSocket;
+var WSS =  window['MozWebSocket'] || ['MozWebSocket'] || ['WebSocket'];
 var ws = new WSS("@routes.Application.videoJs().webSocketURL(request)"); //ref to qiitq 3*  ws.onopen = function () {
   console.log("Openened connection to websocket");
   }
