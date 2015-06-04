9c2a352f25998163d64efdaecd5505efda773a89
ocsi-nanophate <naoyamatayoshi@ocsi.org>
2015-06-04 16:11:35 +0000

    new version javacv and cpp

--- app/controllers/FaceDetect.scala
+++ app/controllers/FaceDetect.scala
@@ -1,10 +1,10 @@
 package javacv
 
-import com.googlecode.javacv.cpp.opencv_core._
-import com.googlecode.javacv.cpp.opencv_imgproc._
-import com.googlecode.javacv.cpp.opencv_highgui._
-import com.googlecode.javacv.cpp.opencv_objdetect._
-import com.googlecode.javacpp.BytePointer
+import javacv.cpp.opencv_core._
+import javacv.cpp.opencv_imgproc._
+import javacv.cpp.opencv_highgui._
+import javacv.cpp.opencv_objdetect._
+import javacpp.BytePointer
 import java.nio.ByteBuffer
 import javax.imageio.ImageIO
 import java.io.ByteArrayOutputStream
